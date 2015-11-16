d14d726702e03e72754c9b6ad4878114ecb1f72e
Thibault Brevet <hi@thibault.io>
2015-11-16 22:03:06 +0100

    comments

--- firmware/constitution_2_go/constitution_2_go.ino
+++ firmware/constitution_2_go/constitution_2_go.ino
@@ -2,13 +2,16 @@
  * CONSTI2GO, 2014, THIBAULT BREVET
  * released under the terms of the MIT License
  *
- * This code takes a char array and streams each characters over the serial comm using ESC/POS commands.
+ * This code takes a char array and streams each characters
+ * over the serial comm using ESC/POS commands.
+ * The board is interfaced with the printer via a MAX3232 chip
+ * (MAX232 works with regular Arduino's on 5V, Teensy 3.1 is 3.3V)
+ *
  * A simple markup language is used for formatting the text:
  *
  * '*' - start inverted mode
  * '%' - stop inverted mode
  * '#' - newline
- *
  */
 
 int push = 12; // push button on pin 12
