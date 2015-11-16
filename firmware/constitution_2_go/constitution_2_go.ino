d9582c5680c63442beef013cdb50046d8b3af1c0
Thibault Brevet <hi@thibault.io>
2015-11-16 21:49:52 +0100

    added comments in code

--- firmware/constitution_2_go/constitution_2_go.ino
+++ firmware/constitution_2_go/constitution_2_go.ino
@@ -1,3 +1,16 @@
+/*
+ * CONSTI2GO, 2014, THIBAULT BREVET
+ * released under the terms of the MIT License
+ *
+ * This code takes a char array and streams each characters over the serial comm using ESC/POS commands.
+ * A simple markup language is used for formatting the text:
+ *
+ * '*' - start inverted mode
+ * '%' - stop inverted mode
+ * '#' - newline
+ *
+ */
+
 int push = 12; // push button on pin 12
 
 void setup ()
